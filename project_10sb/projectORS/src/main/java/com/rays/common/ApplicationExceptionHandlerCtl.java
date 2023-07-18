c7e676d4a0b90a5dee79e62a5f245151484f2d36
sagarpatne <sagarpatne111s@gmail.com>
2023-07-18 15:52:09 +0530

    s1

---
+++ project_10sb/projectORS/src/main/java/com/rays/common/ApplicationExceptionHandlerCtl.java
@@ -0,0 +1,22 @@
+package com.rays.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Handles application propagated exceptions
+ * 
+ * @author SAGAR PATNE
+ *
+ */
+@ControllerAdvice // handle exception globally
+public class ApplicationExceptionHandlerCtl {
+
+	@ExceptionHandler(value = RuntimeException.class) // custom exception handle
+	public ORSResponse handleRuntimeException(RuntimeException e) {
+		ORSResponse res = new ORSResponse(false);
+		res.addMessage(e.getMessage() + "Pushpendra Singh Kushwah");
+		return res;
+	}
+
+}
